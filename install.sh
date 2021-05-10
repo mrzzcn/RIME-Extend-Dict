accc795981ef4e28f3cd0be9e6b1eece085b4054
Jack <mrzzcn@gmail.com>
2021-05-10 17:55:56 +0800

    install script

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/usr/local/bin/bash
 
+token="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"
+
+release=$(curl -H "Authorization: token $token" https://api.github.com/repos/mrzzcn/RIME-Extend-Dict/releases/latest | grep 'Dicts' | grep 'browser_' | cut -d\" -f4);
+
+curl -L -H "Authorization: token $token" "$release" --output Dicts.zip
+
+unzip -qq Dicts.zip
+rm Dicts.zip
+
 cp -r luna_pinyin.extended $HOME/Library/Rime/
 cp luna_pinyin.extended.dict.yaml $HOME/Library/Rime/
-cp luna_pinyin_simp.custom.yaml $HOME/Library/Rime/
 
-osascript -e 'tell application "System Events" to key code 50 using {control down, option down}'
+osascript -e '
+display notification "Download & Install successfully, Deploy by Click Icon -> Deploy." with title "Squirrel"
+'
