521358ac947fa2b51be0a7c23f59d8ae92a6f15b
Jack <mrzzcn@gmail.com>
2021-05-11 15:02:31 +0800

    feat: use public assets link

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/usr/local/bin/bash
 
-token="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"
-
-release=$(curl -H "Authorization: token $token" https://api.github.com/repos/mrzzcn/RIME-Extend-Dict/releases/latest | grep 'Dicts' | grep 'browser_' | cut -d\" -f4);
-
-curl -L -H "Authorization: token $token" "$release" --output Dicts.zip
+curl -L https://github.com/mrzzcn/RIME-Extend-Dict/releases/latest/download/Dicts.zip --output Dicts.zip
 
 unzip -qq Dicts.zip
 rm Dicts.zip
